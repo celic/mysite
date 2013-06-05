32e5d109bd9afaf436230dab0101d3528abb1bb1
celic <cceli@codequarry.net>
2013-06-05 17:47:47 -0400

    Begins work on views

--- polls/views.py
+++ polls/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from django.http import HttpResponse
+from polls.models import Poll
+
+def index(request):
+	latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+	output = ', '.join([p.question for p in latest_poll_list])
+	return HttpResponse(output)
+
+def detail(request):
+	return HttpResponse("You are looking at poll %s." % poll_id)
+
+def results(request):
+	return HttpResponse("You are looking at the results for poll %s." % poll_id)
+
+def vote(request):
+	return HttpResponse("You are voting on poll %s." % poll_id)
