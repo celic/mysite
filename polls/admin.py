9c779d2ac64ac25e8378bc3737a20ae7431c823e
celic <cceli@codequarry.net>
2013-06-05 17:16:06 -0400

    Finishes admin work

--- polls/admin.py
+++ polls/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
-from polls.models import Poll
+from polls.models import Poll, Choice
+
+class ChoiceInLine(admin.TabularInline):
+	model = Choice
+	extra = 3
 
 class PollAdmin(admin.ModelAdmin):
 	fieldsets = [
 		(None, 							 {'fields': ['question']}),
 		('Date Information', {'fields': ['pub_date']})
 	]
+	inlines = [ChoiceInLine]
+	list_display = ('question', 'pub_date', 'was_published_recently')
+	list_filter = ['pub_date']
+	search_fields = ['question']
+	date_heirarchy = 'pub_date'
 
 admin.site.register(Poll, PollAdmin)
