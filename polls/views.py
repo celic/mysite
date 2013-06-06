f455578742df41c9d424c7656e92585364e69783
celic <cceli@codequarry.net>
2013-06-06 18:13:18 -0400

    Finishes views

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
-from django.http import Http404
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from polls.models import Poll
+from django.core.urlresolvers import reverse
+from polls.models import Poll, Choice
 
 def index(request):
 	latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
@@ -11,8 +12,18 @@ def detail(request, poll_id):
 	poll = get_object_or_404(Poll, pk = poll_id)
 	return render(request, 'polls/detail.html', {'poll': poll})
 
-def results(request):
-	return HttpResponse("You are looking at the results for poll %s." % poll_id)
+def results(request, poll_id):
+	poll = get_object_or_404(Poll, pk = poll_id)
+	return render(request, 'polls/results.html', {'poll': poll})
 
-def vote(request):
-	return HttpResponse("You are voting on poll %s." % poll_id)
+def vote(request, poll_id):
+	p = get_object_or_404(Poll, pk = poll_id)
+	try:
+		selected_choice = p.choice_set.get(pk = request.POST['choice'])
+	except(KeyError, Choice.DoesNotExist):
+		context = {'poll': p, 'error_message': "You didn't select a choice"}
+		return render(request, 'polls/detail.html', context)
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
